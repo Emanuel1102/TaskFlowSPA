61f86f0e0f28d9207f17c3f10c7ae53eb8d22faa
Emanuel Manotas <emanuelmanotas2002@gmail.com>
2026-06-05 09:33:30 -0500

    i added the validation in the verifyUser function

--- client/src/services/users.service.js
+++ client/src/services/users.service.js
@@ -12,9 +12,26 @@ export const createUser = async (user) =>{
 
 export const verifyUser = async (email, password=null) => {
     const url = password ? `${userEndpoint}?email=${email}&password=${password}` : `${userEndpoint}?email=${email}`    
-    const response = await fetch(url)
-    const [ user ] = await response.json()
-    return user
+    try {
+        const response = await fetch(url)
+        
+        if (!response.ok) {
+            throw new Error('Error al verificar el usuario')
+        }
+
+        const [ user ] = await response.json()
+
+        if (!user) {
+            return null
+        }
+
+        return user
+        
+    } catch (error) {
+
+        alert(`Error al verificar el usuario: ${error.message}`)
+        
+    }
 }
 
 export const updateUser = async (id, user) => {
